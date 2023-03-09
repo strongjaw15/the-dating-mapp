18872c320a17700e0f4b5dafe8d4e6af917c1b78
Fozzy Yeti <christopherrclark@gmail.com>
2023-03-09 14:10:50 -0600

    Alterted the Header and created the Footer.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,28 @@
+// import { useEffect } from 'react'
 import cookie from 'js-cookie'
 
 const Header = ({user}) => {
 
   const logout = () => {
     cookie.remove('auth-token')
-    window.location.href = '/login'
+    window.location.href = '/'
   }
+
+  // useEffect(() => {
+  //   const authToken = cookie.get('auth-token');
+  //   if (authToken && !user) {
+  //     window.location.href = '/';
+  //     }
+  //     }, [user]);
 
   return (
     <header className="px-2 pb-0 mb-0" style={{ borderBottom: "1px solid #333" }}>
+      <h1>Dating Mapp</h1>
       <nav className="navbar navbar-dark navbar-expand-md bg-body-secondary" data-bs-theme="dark">
         <div className="container-fluid">
+          <head>
+            <title>Dating Mapp</title>
+          </head>
           {/* <a className="navbar-brand" href="##">Navbar</a> */}
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -24,7 +36,7 @@ const Header = ({user}) => {
               { !user ? (
                 <>
                   <li className="nav-item">
-                    <a className="nav-link" href="/signup">Signup</a>
+                    <a className="nav-link" href="/signup">Get Started</a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="/login">Login</a>
@@ -36,7 +48,7 @@ const Header = ({user}) => {
                     <a className="nav-link" href="/profile">Profile</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="##" onCLick={logout}>Logout</a>
+                    <a className="nav-link" href="##" onClick={logout}>Logout</a>
                   </li>
                 </>
               )}
@@ -49,5 +61,5 @@ const Header = ({user}) => {
   )
 }
 
-
 export default Header
+
