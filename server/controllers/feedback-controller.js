8b291519ebbfe24aa2b1db0e382acb6dec8e8849
erik814 <erikneafus@gmail.com>
2023-03-10 10:36:26 -0600

    Added feedback controller and routes

---
+++ server/controllers/feedback-controller.js
@@ -0,0 +1,44 @@
+const Feedback = require('../models/Feedback');
+
+module.exports = {
+    getFeedbacks (req, res) {
+        Feedback.find()
+            .then((feedbacks) => res.json(feedbacks))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    getSingleFeedback(req, res) {
+        Feedback.findOne({ _id: req.params.feedbackId })
+            .select('-__v')
+            .populate('reactions')
+            .then((feedback) =>
+                !feedback
+                    ? res.status(404).json({ message: 'No feedback with that ID' })
+                    : res.json(feedback)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    createFeedback(req, res) {
+        Feedback.create(req.body)
+            .then((feedback) =>
+                User.findOneAndUpdate(
+                    { users: feedback.username },
+                    { $set: {feedbacks: feedback}},
+                    { new: true }
+                )
+            )
+            .then((user) =>
+            !user
+            ? res.status(404).json({
+                message: 'Feedback created, but no user found',
+                })
+            : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+}
+
