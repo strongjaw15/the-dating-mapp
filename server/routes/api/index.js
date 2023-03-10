20cf1dd5dffbf73e50529e2630563551efc4d9a8
erik814 <erikneafus@gmail.com>
2023-03-10 13:50:50 -0600

    Started setting up interest controller.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const feedbackRoutes = require('./feedback-routes');
+const interestRoutes = require('./interest-routes');
 
 router.use('/user', userRoutes);
-
 router.use('/feedback', feedbackRoutes);
+router.use('/interest', interestRoutes);
 
 module.exports = router;
