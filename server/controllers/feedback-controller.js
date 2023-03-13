795dea1640817aea04dc9a29cd4da6efef736e62
Greg Trahan <trahan.greg@gmail.com>
2023-03-13 14:29:12 -0500

    associate interests to user

--- server/controllers/feedback-controller.js
+++ server/controllers/feedback-controller.js
@@ -1,56 +1,55 @@
-const Feedback = require('../models/Feedback');
-const User = require('../models/User');
+const Feedback = require("../models/Feedback");
+const User = require("../models/User");
 
 module.exports = {
-    getFeedbacks (req, res) {
-        Feedback.find()
-            .then((feedbacks) => res.json(feedbacks))
-            .catch((err) => res.status(500).json(err));
-    },
+  getFeedbacks(req, res) {
+    Feedback.find()
+      .then((feedbacks) => res.json(feedbacks))
+      .catch((err) => res.status(500).json(err));
+  },
 
-    getSingleFeedback(req, res) {
-        Feedback.findOne({ _id: req.params.feedbackId })
-            .select('-__v')
-            // .populate('reactions')
-            .then((feedback) =>
-                !feedback
-                    ? res.status(404).json({ message: 'No feedback with that ID' })
-                    : res.json(feedback)
-            )
-            .catch((err) => res.status(500).json(err));
-    },
+  getSingleFeedback(req, res) {
+    Feedback.findOne({ _id: req.params.feedbackId })
+      .select("-__v")
+      // .populate('reactions')
+      .then((feedback) =>
+        !feedback
+          ? res.status(404).json({ message: "No feedback with that ID" })
+          : res.json(feedback)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
-    createFeedback(req, res) {
-        console.log(req.body)
-        Feedback.create(req.body)
-            .then((feedback) =>
-                User.findOneAndUpdate(
-                    { _id: req.body.user },
-                    { $push: { feedbacks: feedback } },
-                    { new: true }
-                )
-            )
-            .then((user) =>
-                !user
-                    ? res.status(404).json({
-                        message: 'Feedback created, but no user found',
-                    })
-                    : res.json(user)
-            )
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-    },
+  createFeedback(req, res) {
+    console.log(req.body);
+    Feedback.create(req.body)
+      .then((feedback) =>
+        User.findOneAndUpdate(
+          { _id: req.body.user },
+          { $push: { feedbacks: feedback } },
+          { new: true }
+        )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Feedback created, but no user found",
+            })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
-    deleteFeedback(req, res) {
-        Feedback.findByIdAndDelete({ _id: req.params.feedbackId })
-            .then((feedback) =>
-                !feedback
-                    ? res.status(404).json({ message: 'No feedback with that ID' })
-                    : res.json(feedback)
-            )
-            .catch((err) => res.status(500).json(err));
-    }
-}
-
+  deleteFeedback(req, res) {
+    Feedback.findByIdAndDelete({ _id: req.params.feedbackId })
+      .then((feedback) =>
+        !feedback
+          ? res.status(404).json({ message: "No feedback with that ID" })
+          : res.json(feedback)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
