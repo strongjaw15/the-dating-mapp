09160bb1e57e993c3bf6d9cb4d990a2bd117821b
danibano <banovetz.danielle@gmail.com>
2023-03-13 14:30:09 -0500

    tiny changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,10 +25,11 @@ const Header = ({user}) => {
         </div>
         <nav className="navbar navbar-light navbar-expand-md bg-body-secondary" data-bs-theme="light">
           <div className="container-fluid">
-            <div>
+            {/* <div>
               <title>Dating Mapp</title>
-            </div>
-            {/* <a className="navbar-brand" href="##">Navbar</a> */}
+            </div> */}
+            {/* <a className="navbar-brand" href="##">Dating Mapp</a>
+            <img src={logo} alt='map' className='logo' /> */}
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -72,4 +73,3 @@ const Header = ({user}) => {
 }
 
 export default Header
-
