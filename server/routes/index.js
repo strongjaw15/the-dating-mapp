fdb2b057ccefb6518443a998c8d10c5b60a46cf0
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-03-14 13:48:30 -0500

    Rolled back routes index file.

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,9 @@
 
 // router.use('/api', apiRoutes);
 
-// router.use('/', function (req, res) {
+// // Define a catch-all route that will handle all requests
+// router.get('*', function (req, res) {
+//   res.setHeader('Content-Disposition', 'inline');
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
@@ -12,14 +14,7 @@
 
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const path = require('path')
 
 router.use('/api', apiRoutes);
-
-// Define a catch-all route that will handle all requests
-router.get('*', function (req, res) {
-  res.setHeader('Content-Disposition', 'inline');
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 module.exports = router;
