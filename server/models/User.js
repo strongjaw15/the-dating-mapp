3ccd1327d8f6ec346419a17b1203969874b5de2b
Fozzy Yeti <christopherrclark@gmail.com>
2023-03-14 11:45:48 -0500

    worked on new seed files

--- server/models/User.js
+++ server/models/User.js
@@ -28,6 +28,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  interestScore: {
+    type: Number,
+    required: true,
+  },
 
   interests: [
     {
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
-require('../config/connection');
 
-const seedUser = require('./user-seeds');
-const seedFeedback = require('./feedback-seeds');
-const seedInterest = require('./interest-seeds');
+const db = require('../config/connection');
+const { User, Feedback, Interest, Location } = require('../models');
 
+const seedUser = require('./userData');
+const seedFeedback = require('./feedbackData');
+const seedInterest = require('./interestData');
+const seedLocation = require('./locationData');
 
-const seedAll = async () => {
-    await seedUser();
-    console.log('\n----- Users SEEDED -----\n');
+db,once('open', async () => {
 
-    await seedFeedback();
-    console.log('\n----- Feedback SEEDED -----\n');
+    const users = await User.insertMany(seedUser);
+    const feedback = await Feedback.insertMany(seedFeedback);
+    const interests = await Interest.insertMany(seedInterest);
+    const locations = await Location.insertMany(seedLocation);
 
-    await seedInterest();
-    console.log('\n----- Interests SEEDED -----\n');
-
+    console.log('Users seeded!');
+    console.log('Feedback seeded!');
+    console.log('Interests seeded!');
+    console.log('Locations seeded!');
     process.exit(0);
-};
+})
 
 seedAll();
