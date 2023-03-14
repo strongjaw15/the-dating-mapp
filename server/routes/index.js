63cf0b27d13c5d585e7ae33abb056535d48cbae5
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-03-14 12:15:29 -0500

    Added build route to route index.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,8 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.use('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 module.exports = router;
