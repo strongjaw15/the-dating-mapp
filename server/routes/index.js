2735b4b41c85a565cb5b9e9ef0ab47e27b572d3a
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-03-14 12:24:38 -0500

    Modified routes index.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
+// const router = require('express').Router();
+// const apiRoutes = require('./api');
+// const path = require('path')
+
+// router.use('/api', apiRoutes);
+
+// router.use('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// module.exports = router;
+
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const path = require('path')
 
 router.use('/api', apiRoutes);
 
-router.use('/', function (req, res) {
+// Define a catch-all route that will handle all requests
+router.get('*', function (req, res) {
+  res.setHeader('Content-Disposition', 'inline');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
